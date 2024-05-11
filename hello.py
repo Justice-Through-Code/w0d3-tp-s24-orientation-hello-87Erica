39934f56a6b67286733cb08f75c74453608feab2
Erica Graham <grahamerica04@gmail.com>
2024-05-11 15:27:32 -0400

    Implement hello_world() function

--- hello.py
+++ hello.py
@@ -1,10 +1,14 @@
 def hello_world():
     # Remove the # in the line below and delete the word pass
-    # print("Hello World!")
+    print("Hello World!")
 
-    pass
-   
+greeting = hello_world()
+print(greeting)
+
+      
 
 # Do not modify the code below this line
 if __name__ == "__main__":
     hello_world()
+
+ 
